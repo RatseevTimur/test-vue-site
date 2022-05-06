a5c70b48e0acd02c54d017de5484175afee1ecd2
RatseevTimur <rtimsh@yandex.ru>
2022-05-06 23:37:30 +0300

    component-product-review

--- main.js
+++ main.js
@@ -1,3 +1,71 @@
+Vue.component('product-review', {
+  template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+    
+      <p class="error" v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name">
+      </p>
+      
+      <p>
+        <label for="review">Review:</label>      
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+      
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+          
+      <p>
+        <input type="submit" value="Submit">  
+      </p>    
+    
+  </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: []
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.errors = []
+      if(this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating
+        }
+        this.$emit('review-submitted', productReview)
+        this.name = null
+        this.review = null
+        this.rating = null
+      } else {
+        if(!this.name) this.errors.push("Name required.")
+        if(!this.review) this.errors.push("Review required.")
+        if(!this.rating) this.errors.push("Rating required.")
+      }
+    }
+  }
+})
+
 Vue.component('product', {
   props: {
     premium: {
@@ -6,124 +74,133 @@ Vue.component('product', {
     }
   },
   template: `
-    <div class="product">
-
+   <div class="product">
+        
       <div class="product-image">
         <img :src="image" />
       </div>
 
       <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
+          <h1>{{ product }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+          <p>Shipping: {{ shipping }}</p>
 
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
+          <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+          </ul>
 
           <div class="color-box"
-          v-for="(variant, index) in variants" 
-          :key="variant.variantId"
-          :style="{ backgroundColor: variant.variantColor }"
-          @mouseover="updateProduct(index)"
-          >
-          
-          <p class="сounter">{{ variant.variantQuantity }}</p>
+               v-for="(variant, index) in variants" 
+               :key="variant.variantId"
+               :style="{ backgroundColor: variant.variantColor }"
+               @mouseover="updateProduct(index)"
+               >
+
+               <p class="сounter">{{ variant.variantQuantity }}</p>
+          </div> 
+
+          <button v-on:click="addToCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >
+          Add to cart
+          </button>
+
+       </div>  
+
+       <div>
+              <p v-if="!reviews.length">There are no reviews yet.</p>
+              <ul v-else>
+                  <li v-for="(review, index) in reviews" :key="index">
+                    <p>{{ review.name }}</p>
+                    <p>Rating:{{ review.rating }}</p>
+                    <p>{{ review.review }}</p>
+                  </li>
+              </ul>
           </div>
-
-      </div> 
-
-      <button v-on:click="addToCart" 
-      :disabled="!inStock"
-      :class="{ disabledButton: !inStock }"
-      >
-      Add to cart
-      </button>
-
-      <button v-on:click="subtractToCart" 
-        :disabled="cart <= 0"
-        :class="{ disabledButton: cart <= 0 }">
-        <span style='font-size:15px;'>&#10006;</span>
-        -1
-      </button>
-
-        <button class="cart" v-on:click="resetToCart">
-          <p class="сounterCart">Cart({{ cart }})</p>
-        </button>
-
+         
+         <product-review @review-submitted="addReview"></product-review>
+    
     </div>
-  `,
+   `,
   data() {
-    return{
-      product: 'Niva',
-      brand: 'Vue Mastery',
-      selectedVariant: 0,
-      details: ["1.7 L","EBPO-5","83 l.c."],
-      variants: [
-      {
-        variantId: 2234,
-        variantColor: '#6e0000',
-        variantImage: '/home/tim/Projects/test-vue-site/img/luxe_172.png',
-        variantQuantity: 10    
-      },
-      {
-        variantId: 2235,
-        variantColor: 'white',
-        variantImage: '/home/tim/Projects/test-vue-site/img/luxe_240.png',
-        variantQuantity: 1
-      },
-      {
-        variantId: 2,
-        variantColor: 'black',
-        variantImage: '/home/tim/Projects/test-vue-site/img/luxe_672.png',
-        variantQuantity: 0
-      },
-      {
-        variantId: 3,
-        variantColor: 'grey',
-        variantImage: '/home/tim/Projects/test-vue-site/img/luxe_633.png',
-        variantQuantity: 3
-      }
-    ],
-    cart: 0
-  }
-},
-methods: {
-  subtractToCart: function(){
-    this.cart -= 1
-  },
-  resetToCart: function(){
-    this.cart = 0
-  },
-  addToCart: function(){
-    this.cart += 1
-  },
-  updateProduct: function(index) {  
-    this.selectedVariant = index
-  }
-},
-  computed: {
-    title() {
-        return this.brand + ' ' + this.product  
-    },
-    image(){
-        return this.variants[this.selectedVariant].variantImage
-    },
-    inStock(){
-        return this.variants[this.selectedVariant].variantQuantity
-    },
-    shipping() {
-      if (this.premium) {
-        return "Free"
-      }
-        return 2.99
+    return {
+        product: 'Legend',
+        brand: 'Niva',
+        selectedVariant: 0,
+        details: ["1.7 L","EBPO-5","83 l.c."],
+        variants: [
+          {
+            variantId: 1,
+            variantColor: '#6e0000',
+            variantImage: 'img/luxe_172.png',
+            variantQuantity: 10    
+          },
+          {
+            variantId: 2,
+            variantColor: 'white',
+            variantImage: 'img/luxe_240.png',
+            variantQuantity: 1
+          },
+          {
+            variantId: 3,
+            variantColor: 'black',
+            variantImage: 'img/luxe_672.png',
+            variantQuantity: 0
+          },
+          {
+            variantId: 4,
+            variantColor: 'grey',
+            variantImage: 'img/luxe_633.png',
+            variantQuantity: 3    
+          }
+        ],
+        reviews: []
     }
-  }
-  })
-  
+  },
+    methods: {
+      addToCart: function() {
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
+      },
+      updateProduct: function(index) {  
+          this.selectedVariant = index
+      }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product  
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping() {
+          if (this.premium) {
+            return "Free"
+          }
+            return 1.28 + "mln"
+        }
+    }
+})
+
 var app = new Vue({
     el: '#app',
     data: {
-      premium: true
+      premium: true,
+      cart: []
+    },
+    methods: {
+      updateCart(id) {
+        this.cart.push(id)
+      },///////////////////////////////////////
+      /*subtractToCart: function(){
+        this.cart.splice(-1,1)
+      },
+      resetToCart: function(){
+        this.cart = 0
+      },*////////////////////////////////////////
     }
-  })
+})
